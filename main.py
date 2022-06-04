d464b43c3f339b8c61db10bd72070ff97b55604c
chfour <michaleklitwin@gmail.com>
2022-06-04 15:43:10 +0200

    reading

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ fuse.fuse_python_api = (0, 2)
 FILES = {"/test": b"Hello, world!"}
 
 class TheFS(fuse.Fuse):
-    def getattr(self, path: str) -> fuse.Stat:
+    def getattr(self, path: str) -> fuse.Stat | int:
         st = fuse.Stat()
         
         if path == "/":
@@ -24,8 +24,29 @@ class TheFS(fuse.Fuse):
         return st
 
     def readdir(self, path, offset) -> Generator[fuse.Direntry, None, None]:
+        print(f"readdir {path} o{offset}")
         for f in [".", ".."] + [e[1:] for e in FILES]:
             yield fuse.Direntry(f)
+
+    def open(self, path, flags) -> None | int:
+        if path not in FILES:
+            print(f"open {path} {flags:b} -> ENOENT")
+            return -errno.ENOENT
+
+        if flags & (os.O_RDONLY | os.O_WRONLY | os.O_RDWR) != os.O_RDONLY:
+            print(f"open {path} {flags:b} -> EACCES")
+            return -errno.EACCES
+        
+        print(f"open {path} {flags:b} -> OK")
+
+    def read(self, path, size, offset) -> bytes | int:
+        if path not in FILES:
+            print(f"read {path} s{size} o{offset} -> ENOENT")
+            return -errno.ENOENT
+
+        buf = FILES[path][offset:size]
+        print(f"read {path} s{size} o{offset} -> bytes[{len(buf)}]")
+        return buf
 
 def main() -> None:
     server = TheFS(dash_s_do="setsingle")
