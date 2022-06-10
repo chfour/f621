c07d703cbc35dbe4581f58b22456a3e2bc9ee0de
chfour <michaleklitwin@gmail.com>
2022-06-10 19:09:10 +0200

    add info files

--- main.py
+++ main.py
@@ -7,15 +7,22 @@ fuse.fuse_python_api = (0, 2)
 
 USERAGENT = "f621/1.0 (github.com/chfour)"
 
+def _size_to_readable(n: int) -> str:
+    i = 1
+    while n / (1024**i) > 1:
+        n = n / (1024**i)
+        i += 1
+    return f"{round(n)} " + [None, "KiB", "MiB", "GiB"][i-1]
 
 class E6Post:
     def __repr__(self) -> str:
         return f"E6Post#{self.data['id']}"
 
     @classmethod
-    def from_json(cls, data: dict) -> "E6Post":
+    def from_json(cls, data: dict, api="https://e621.net") -> "E6Post":
         new_object = cls(None)
         new_object.data = data
+        new_object.api = api
         new_object.post_id = new_object.data["id"]
         new_object.info_request = None
         new_object.images = {}
@@ -23,7 +30,9 @@ class E6Post:
     
     def __init__(self, post_id: int | None, api="https://e621.net") -> None:
         if post_id is None: return
-        images = {}
+        self.images = {}
+
+        self.api = api
         
         self.post_id = post_id
         self.info_request = requests.get(f"{api}/posts/{post_id}.json", headers={"user-agent": USERAGENT})
@@ -50,6 +59,39 @@ class E6Post:
         else:
             return len(self.get_image(fmt=fmt))
 
+    def generate_info(self) -> str:
+        output = f"ID: {self.post_id}\nURL: {self.api}/posts/{self.post_id}\n"
+
+        if len(self.data["description"]) > 0:
+            output += "Description:\n " + "\n ".join(self.data["description"].replace("\r", "").split("\n")) + "\n"
+        else:
+            output += "Description: (none)\n"
+
+        output += "\n"
+        
+        for field in ["artist", "copyright", "character", "species", "lore", "general", "meta", "invalid"]:
+            if len(self.data["tags"][field]):
+                output += {"artist": "Artists", "copyright": "Copyrights", "character": "Characters"}.get(field, field.title()) + ":\n "
+                output += "\n ".join(self.data["tags"][field]) + "\n"
+        
+        output += "\nSource[s]:\n " + "\n ".join(self.data["sources"]) + "\n" + \
+            f"Rating: " + {"s": "Safe", "e": "Explicit", "q": "Questionable"}[self.data["rating"]] + "\n" + \
+            f"Score: {self.data['score']['total']} ({self.data['score']['up']} up/{self.data['score']['down']} down)\n" + \
+            f"Posted: {self.data['created_at']} (updated {self.data['updated_at']})\n" + \
+            f"Size: {self.data['file']['width']}x{self.data['file']['height']} ({_size_to_readable(self.data['file']['size'])})\n" + \
+            f"Favorites: {self.data['fav_count']}\nMD5: {self.data['file']['md5']}\n"
+        
+        if self.data["relationships"]["parent_id"] is not None:
+            output += f"Parent post: {self.data['relationships']['parent_id']}\n"
+        
+        if self.data["relationships"]["has_children"]:
+            output += f"Children: " + " ".join([str(c) for c in self.data["relationships"]["children"]]) + "\n"
+        
+        if len(self.data["pools"]) > 0:
+            output += f"Pools: " + " ".join(self.data["pools"]) + "\n"
+        
+        return output
+
 
 class TheFS(fuse.Fuse):
     api = "https://e621.net"
@@ -73,6 +115,7 @@ class TheFS(fuse.Fuse):
                 post = self.cache[post["id"]]
             else:
                 post = E6Post.from_json(post)
+                post.api = self.api
                 self.cache[post.data["id"]] = post
             page_listing.append(post)
         
@@ -102,8 +145,13 @@ class TheFS(fuse.Fuse):
             st.st_size = len(self.files[path])
             print(f"getattr {path} -> {st.__dict__}")
         elif path.startswith(self.postsdir):
+            st.st_mode = stat.S_IFREG | 0o444
+            st.st_nlink = 1
             try:
-                post_id = int(path[len(self.postsdir):])
+                if "_" in path:
+                    post_id = int(path[len(self.postsdir):path.index("_")])
+                else:
+                    post_id = int(path[len(self.postsdir):])
             except ValueError:
                 print(f"getattr {path} (not int) -> ENOENT")
                 return -errno.ENOENT
@@ -114,14 +162,14 @@ class TheFS(fuse.Fuse):
                 print(f"getattr {path} (error fetching post: {e}) -> ENOENT")
                 return -errno.ENOENT
 
-            image_size = self.files["/size"].decode("utf-8").strip()
-            if image_size not in ["file", "sample", "preview"]:
-                image_size = "sample"
-            self.files["/size"] = (image_size + "\n").encode("utf-8")
-            
-            st.st_mode = stat.S_IFREG | 0o444
-            st.st_nlink = 1
-            st.st_size = post.get_image_size(fmt=image_size)
+            if path.endswith("_info"):
+                st.st_size = len(post.generate_info().encode("utf-8"))
+            else:
+                image_size = self.files["/size"].decode("utf-8").strip()
+                if image_size not in ["file", "sample", "preview"]:
+                    image_size = "sample"
+                self.files["/size"] = (image_size + "\n").encode("utf-8")
+                st.st_size = post.get_image_size(fmt=image_size)
         else:
             print(f"getattr {path} -> ENOENT")
             return -errno.ENOENT
@@ -146,7 +194,8 @@ class TheFS(fuse.Fuse):
             except RuntimeError as e:
                 print(f"{path} (error fetching post: {e}) -> ENOENT")
                 return -errno.EIO
-            dir_listing += [str(p.data["id"]) for p in page]
+            dir_listing += [str(p.post_id) for p in page]
+            dir_listing += [f"{p.post_id}_info" for p in page]
         else:
             print(f"readdir {path} o{offset} -> ENOENT")
             return -errno.ENOENT
@@ -161,7 +210,10 @@ class TheFS(fuse.Fuse):
             return 0
         elif path.startswith(self.postsdir):
             try:
-                post_id = int(path[len(self.postsdir):])
+                if "_" in path:
+                    post_id = int(path[len(self.postsdir):path.index("_")])
+                else:
+                    post_id = int(path[len(self.postsdir):])
             except ValueError:
                 print(f"open {path} (not int) -> ENOENT")
                 return -errno.ENOENT
@@ -189,7 +241,10 @@ class TheFS(fuse.Fuse):
             return buf
         elif path.startswith(self.postsdir):
             try:
-                post_id = int(path[len(self.postsdir):])
+                if "_" in path:
+                    post_id = int(path[len(self.postsdir):path.index("_")])
+                else:
+                    post_id = int(path[len(self.postsdir):])
             except ValueError:
                 print(f"getattr {path} (not int) -> ENOENT")
                 return -errno.ENOENT
@@ -200,18 +255,22 @@ class TheFS(fuse.Fuse):
                 print(f"getattr {path} (error fetching post: {e}) -> ENOENT")
                 return -errno.EIO
 
-            image_size = self.files["/size"].decode("utf-8").strip()
-            if image_size not in ["file", "sample", "preview"]:
-                image_size = "sample"
-            self.files["/size"] = (image_size + "\n").encode("utf-8")
+            if path.endswith("_info"):
+                buf = post.generate_info().encode("utf-8")[offset:offset+size]
+            else:
+                image_size = self.files["/size"].decode("utf-8").strip()
+                if image_size not in ["file", "sample", "preview"]:
+                    image_size = "sample"
+                self.files["/size"] = (image_size + "\n").encode("utf-8")
 
-            try:
-                image_data = post.get_image(fmt=image_size)
-            except RuntimeError as e:
-                print(f"getattr {path} (error fetching post image: {e}) -> ENOENT")
-                return -errno.EIO
+                try:
+                    image_data = post.get_image(fmt=image_size)
+                except RuntimeError as e:
+                    print(f"getattr {path} (error fetching post image: {e}) -> ENOENT")
+                    return -errno.EIO
 
-            buf = image_data[offset:offset+size]
+                buf = image_data[offset:offset+size]
+            
             print(f"read {path} s{size} o{offset} -> bytes[{len(buf)}]")
             return buf
         else:
